47b8be753c2e242a564faf83f1ced0fe9174c74a
fawei <50777393@qq.com>
2022-04-17 22:03:06 +0800

    dian - order schedule

---
+++ WPF-Miao/HttpProcessor/JsonFactory/JsonStringConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HttpProcessor.JsonFactory
+{
+    public class JsonStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type typeToConvert) => typeToConvert == typeof(long) || typeToConvert == typeof(long?);
+    }
+}
---
+++ WPF-Miao/ProcessModel/Order/OrderScheduleModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessModel.Order
+{
+    public abstract class OrderScheduleModel
+    {
+    }
+}
