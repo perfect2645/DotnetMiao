ce188deab9398f94f3c63100bc9a5583b581cc45
fawei <50777393@qq.com>
2022-05-01 23:13:29 +0800

    dian - httpclientcontentbase

---
+++ WPF-Miao/HttpProcessor/Client/HttpClientContentBase.cs
@@ -0,0 +1,17 @@
+﻿using HttpProcessor.JsonFactory;
+using System.Text;
+using System.Text.Json;
+
+namespace HttpProcessor.Client
+{
+    public abstract class HttpClientContentBase
+    {
+        private readonly Dictionary<string, object> _contents = new Dictionary<string, object>();
+
+        public StringContent GetJsonContent()
+        {
+            var jsonString = JsonSerializer.Serialize(_contents, JsonEncoder.JsonOption);
+            return new StringContent(jsonString, Encoding.UTF8, "application/json");
+        }
+    }
+}
--- WPF-Miao/WPF-Miao/Platform/DianYiTong/Submit/SubmitContent.cs
+++ WPF-Miao/WPF-Miao/Platform/DianYiTong/Submit/SubmitContent.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using HttpProcessor.Client;
+using HttpProcessor.JsonFactory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace WPF_Miao.Platform.DianYiTong.Submit
 {
-    internal class SubmitContent
+    internal class SubmitContent : HttpClientContentBase
     {
-        private Dictionary<string, object> _contents = new Dictionary<string, object>();
-
-
     }
 }
