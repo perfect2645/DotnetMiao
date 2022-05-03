e72bfbff08eb9d8a844cc6949a797cf057da3365
fawei <50777393@qq.com>
2022-05-03 21:00:51 +0800

    httpProcessor - httpexception

---
+++ WPF-Miao/HttpProcessor/Exception/HttpException.cs
@@ -0,0 +1,12 @@
+﻿namespace HttpProcessor.ExceptionManager
+{
+    public class HttpException : Exception
+    {
+        public int StatusCode { get; private set; }
+
+        public HttpException(string message, int statusCode = 200): base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
