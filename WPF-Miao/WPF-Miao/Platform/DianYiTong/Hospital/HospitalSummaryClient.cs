674195150079df9e21866669a153056a437169f3
Fawei Cui <perfect200@163.com>
2022-05-22 13:18:59 +0800

    dian - catch hospital info

--- WPF-Miao/WPF-Miao/Platform/DianYiTong/Hospital/HospitalSummaryClient.cs
+++ WPF-Miao/WPF-Miao/Platform/DianYiTong/Hospital/HospitalSummaryClient.cs
@@ -1,4 +1,5 @@
 ﻿using HttpProcessor.Client;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,8 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Utils;
+using WPF_Miao.Platform.DianYiTong.Session;
 
 namespace WPF_Miao.Platform.DianYiTong.Hospital
 {
@@ -25,8 +28,23 @@ namespace WPF_Miao.Platform.DianYiTong.Hospital
 
         private void PostSearch(HttpDicResponse dicResponse)
         {
-            var data = dicResponse.Body["data"];
-            var dateList = dicResponse.Body["date_list"];
+            var bodyRoot = dicResponse.JsonBody.RootElement;
+            var data = bodyRoot.GetProperty("data");
+
+            if (data.ValueKind == JsonValueKind.Array)
+            {
+                foreach (JsonElement prop in data.EnumerateArray())
+                {
+                    var jsonStr = prop.ToString();
+                    var contentDic = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonStr);
+                    if (contentDic == null)
+                    {
+                        continue;
+                    }
+                    var doctorId = prop.GetProperty("doc_id").GetInt32().ToString();
+                    DianSession.SessionData.AddOrUpdate(doctorId, contentDic);
+                }
+            }
         }
 
         #endregion Search
--- WPF-Miao/WPF-Miao/Platform/DianYiTong/Session/DianSession.cs
+++ WPF-Miao/WPF-Miao/Platform/DianYiTong/Session/DianSession.cs
@@ -5,11 +5,15 @@ namespace WPF_Miao.Platform.DianYiTong.Session
 {
     internal static class DianSession
     {
+        static DianSession()
+        {
+            SessionData = new Dictionary<string, object>();
+        }
         public static OrderRequest OrderRequest { get; set; }
 
         #region Required Info
 
-        public static Dictionary<string, object> Session { get; set; }
+        public static Dictionary<string, object> SessionData { get; set; }
 
 
         #endregion Required Info
