acf32d2aa0005e3dac203c5aa5d7318d6c36df0f
fawei <50777393@qq.com>
2022-07-23 21:18:12 +0800

    dian - view

---
+++ WPF-Miao/Dian/Initializer.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+
+namespace Dian
+{
+    internal static class Initializer
+    {
+        public static async Task InitAsync()
+        {
+            await Task.Factory.StartNew(() => InitHttpContainer());
+            await Task.Factory.StartNew(() => InitViewContainer());
+        }
+        private static void InitHttpContainer()
+        {
+/*            //var handler = new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.GZip };
+            HttpServiceController.AddTransientService<UserInfoController, AppointmentHandler>();
+            HttpServiceController.ServiceCollection.AddTransient<UserInfoContent>();
+
+            HttpServiceController.AddTransientService<AppointmentController, AppointmentHandler>();
+            HttpServiceController.AddTransientService<GetTimestampController>();
+            HttpServiceController.ServiceCollection.AddTransient<AppointmentContent>();
+            HttpServiceController.ServiceCollection.AddTransient<SecureHeader>();
+            HttpServiceController.ServiceCollection.BuildServiceProvider();
+            HttpServiceController.BuidServiceProvider();*/
+        }
+
+        private static void InitViewContainer()
+        {
+/*            Container.ServiceCollection.AddTransient<ISessionItem, SessionItem>();
+            Container.BuildServiceProvider();*/
+        }
+    }
+}
