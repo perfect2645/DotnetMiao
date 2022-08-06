0a83b7717b5d5f5d1eedf6bd1d6c5940e00f1b44
Fawei Cui <perfect200@163.com>
2022-08-06 16:15:08 +0800

    base session events

---
+++ WPF-Miao/Baohe/search/SearchController.cs
@@ -0,0 +1,19 @@
+﻿using HttpProcessor.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baohe.search
+{
+    internal class SearchController : HttpClientBase
+    {
+        public SearchController(HttpClient httpClient) : base(httpClient)
+        {
+
+            
+        }
+    }
+}
---
+++ WPF-Miao/Base/Events/SessionEvents.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Events
+{
+    public class SessionEvents
+    {
+        #region Handler
+
+        public event EventHandler<SesstionEventArgs> SessionEventHandler;
+
+        #endregion Handler
+
+        #region Subscribe
+
+        public void Subscribe(EventHandler<SesstionEventArgs> handler)
+        {
+            SessionEventHandler += handler;
+        }
+
+        #endregion Subscribe
+
+        #region Publish
+
+        public void Publish(object? sender, SesstionEventArgs args)
+        {
+            SessionEventHandler?.Invoke(sender, args);
+        }
+
+        #endregion Publish
+    }
+
+    public class SesstionEventArgs : EventArgs
+    {
+        public Dictionary<string, object> Session { get; set; }
+    }
+}
