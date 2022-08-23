de9d4e91aeb1450d36bed6d66639cdfa0baf3f63
50777393@qq.com <50777393@qq.com>
2022-08-24 00:47:02 +0800

    order

---
+++ WPF-Miao/Baohe/appointment/Order.cs
@@ -0,0 +1,83 @@
+﻿namespace Baohe.appointment
+{
+    internal class Order
+    {
+        #region properties
+
+        public string OrderKey 
+        { 
+           get { return $"{MemberSn}_{NumberSn}"; }
+        }
+
+        public string MemberSn { get; }
+        public string NumberSn { get; }
+
+        public OrderStatus Status { get; set; }
+
+        public AppointmentContent Content { get; set; }
+
+        #endregion properties
+
+        #region Constructor
+
+        public Order(string memberSn)
+        {
+            MemberSn = memberSn;
+
+            OrderStatusEvent.OrderStatusChangedEvent += OrderStatusEvent_OrderStatusChangedEvent;
+        }
+
+        public Order(string memberSn, string numberSn)
+        {
+            MemberSn = memberSn;
+            NumberSn = numberSn;
+            OrderKey = $"{memberSn}_{numberSn}";
+
+            OrderStatusEvent.OrderStatusChangedEvent += OrderStatusEvent_OrderStatusChangedEvent;
+        }
+
+        #endregion Constructor
+
+        #region BuildContent
+
+        public void InitContent()
+        {
+
+        }
+
+        public void FillContent()
+        {
+            Appoint();
+        }
+
+        #endregion BuildContent
+
+        #region Appointment
+
+        private void Appoint()
+        {
+            if (Status == OrderStatus.Pending)
+            {
+                Status = OrderStatus.Running;
+                // publish to appointmentcontroller
+            }
+
+        }
+
+        #endregion Appointment
+
+        #region Status
+
+        private void OrderStatusEvent_OrderStatusChangedEvent(object? sender, OrderStatusArgs e)
+        {
+            if (e.OrderKey != OrderKey)
+            {
+                return;
+            }
+
+            Status = e.Status;
+        }
+
+        #endregion Status
+    }
+}
---
+++ WPF-Miao/Baohe/appointment/OrderStatus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Baohe.appointment
+{
+    internal enum OrderStatus
+    {
+        Pending,
+        Running,
+        Failed,
+        Cancelled,
+    }
+
+    internal class OrderStatusEvent
+    {
+        internal static event EventHandler<OrderStatusArgs> OrderStatusChangedEvent;
+
+        internal void Subscribe(EventHandler<OrderStatusArgs> handler)
+        {
+            OrderStatusChangedEvent += handler;
+        }
+
+        internal void Publish(object? sender, OrderStatusArgs e)
+        {
+            OrderStatusChangedEvent?.Invoke(sender, e);
+        }
+    }
+
+    internal class OrderStatusArgs : EventArgs
+    {
+        public string OrderKey { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public OrderStatusArgs(string orderKey, OrderStatus status)
+        {
+            OrderKey = orderKey;
+            Status = status;
+        }
+    }
+}
