49d92dadb8148c3ab71f6d5685d9a57baec8ea25
Fawei Cui <perfect200@163.com>
2022-09-12 12:39:01 +0800

    jk160 - initial commit

---
+++ WPF-Miao/Jk160/view/Jk160Console.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Jk160.view
+{
+    /// <summary>
+    /// Jk160Console.xaml 的交互逻辑
+    /// </summary>
+    public partial class Jk160Console : Page
+    {
+        public Jk160Console()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPF-Miao/Jk160/viewmodel/Jk160ViewModel.cs
@@ -0,0 +1,175 @@
+﻿using Base.Events;
+using Base.viewModel;
+using Base.viewModel.hospital;
+using CoreControl.LogConsole;
+using HttpProcessor.Container;
+using HttpProcessor.ExceptionManager;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Utils;
+using Utils.datetime;
+using Utils.stringBuilder;
+
+namespace Baohe.viewModel
+{
+    internal class Jk160ViewModel: ViewModelBase
+    {
+        #region Properties
+
+        public ICommand AppointmentCommand { get; set; }
+        public ICommand SearchCommand { get; set; }
+
+        public ICommand AutoRunCommand { get; set; }
+
+        public VerifyCode VerifyCode { get; set; }
+
+        #endregion Properties
+
+        #region Constructor
+
+        public Jk160ViewModel(LogPanel logPanel) : base(logPanel)
+        {
+            VerifyCode = new VerifyCode(logPanel);
+            InitStaticData();
+            InitCommands();
+            BaoheSession.PrintLogEvent = PrintLogEvent;
+        }
+
+        private void InitStaticData()
+        {
+            Departments = new List<HospitalDept>();
+            Departments.Add(new Jiankangzhilu("9001026", "蜀山区井岗中心服务号",
+                "1047063", "蜀山区经开区井岗镇社区卫生服务中心",
+                "7229244", "四价Hpv"));
+            Departments.Add(new Jiankangzhilu("9001026", "蜀山区井岗中心服务号",
+                "1047063", "蜀山区经开区井岗镇社区卫生服务中心",
+                "7209050", "(测试)儿童保健科"));
+
+
+            InitPlatformSession();
+        }
+
+        private void InitPlatformSession()
+        {
+            try
+            {
+                var tsStr = DateTimeUtil.GetTimeStamp();
+                var sessionTime = tsStr.Substring(0, 10);
+                BaoheSession.PlatformSesstion.Add(Constant.SessionTime, sessionTime);
+            }
+            catch (HttpException ex)
+            {
+                Log(ex);
+            }
+            catch (Exception ex)
+            {
+                Log(ex);
+            }
+        }
+
+        private void InitCommands()
+        {
+            AppointmentCommand = new DelegateCommand(ExecuteAppointment, CanExecuteAppointment);
+            SearchCommand = new DelegateCommand(ExecuteSearchAsync, CanExecuteSearch);
+
+            AutoRunCommand = new DelegateCommand(ExecuteAutoRun);
+
+            SessionEvents.Instance.Subscribe(LogSession);
+
+            SelectedDepartmentChanged = new Action(OnSelectedDepartmentChanged);
+        }
+
+        #endregion Constructor
+
+        #region Appointment
+
+        private bool CanExecuteAppointment()
+        {
+            return true;
+        }
+
+        private void ExecuteAppointment()
+        {
+
+            var appRouter = new AppointmentRouter(SessionItem);
+
+            appRouter.AppointTickAsync();
+        }
+
+
+        #endregion Appointment
+
+        #region Search
+
+        private bool CanExecuteSearch()
+        {
+            return true;
+        }
+
+        private async void ExecuteSearchAsync()
+        {
+            try
+            {
+                var searchController = HttpServiceController.GetService<SearchController>();
+                await searchController.SearchAllAsync(SessionItem);
+            }
+            catch (HttpException ex)
+            {
+                Log(ex);
+            }
+            catch (Exception ex)
+            {
+                Log(ex);
+            }
+        }
+
+        #endregion Search
+
+        #region AutoRun
+
+        private void ExecuteAutoRun()
+        {
+            BaoheSession.Cookie = SessionItem.Cookie;
+            Task.Factory.StartNew(async () =>
+            {
+                await AutoRunAsync();
+            });
+        }
+
+        private async Task AutoRunAsync()
+        {
+            var searchController = HttpServiceController.GetService<SearchController>();
+            await searchController.AutoSearchAsync(SessionItem);
+        }
+
+        #endregion AutoRun
+
+        #region Session
+
+        private void LogSession(object? sender, SesstionEventArgs args)
+        {
+
+        }
+
+        #endregion Session
+
+        #region Hospital Dept
+
+        private void OnSelectedDepartmentChanged()
+        {
+            var selectedDept = SelectedDepartment as Jiankangzhilu;
+            BaoheSession.PlatformSesstion.AddOrUpdate(Constant.PlatformType, selectedDept.PlatformId);
+            BaoheSession.PlatformSesstion.AddOrUpdate(Constant.HospitalId, selectedDept.HospitalId);
+            BaoheSession.PlatformSesstion.AddOrUpdate(Constant.DeptId, selectedDept.DepartmentId);
+
+            Log(selectedDept.ToLogString());
+
+            BaoheSession.BuildMiaoSession(BaoheSession.PlatformSesstion[Constant.DeptId].NotNullString());
+        }
+
+        #endregion Hospital Dept
+
+    }
+}
