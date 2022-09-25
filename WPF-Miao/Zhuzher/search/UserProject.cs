41a2ff523bf87143e44a27b3704e061094db3e5f
50777393@qq.com <50777393@qq.com>
2022-09-25 19:33:31 +0800

    baba auth

--- WPF-Miao/Zhuzher/search/UserProject.cs
+++ WPF-Miao/Zhuzher/search/UserProject.cs
@@ -28,7 +28,7 @@ namespace Zhuzher.search
             //AddUserProject(15045709, "妈妈", "21020025", "大连万科半山半海", "Bearer d19fc1cd-7efd-4bb2-a1cb-af42d92cf051");
             //AddUserProject(11067046, "Fawei", "21020002", "大连假日风景", "Bearer bcb0e3b8-2f3d-4a8e-b7f3-2e1782423321");
             //AddUserProject(14929048, "Gary", "21020012", "大连樱花园", "Bearer d19fc1cd-7efd-4bb2-a1cb-af42d92cf051");
-            //AddUserProject(21744666, "爸爸", "21020002", "大连假日风景", "Bearer b26ed045-4534-452e-a595-dd8559c946db");
+            //AddUserProject(21744666, "爸爸", "21020002", "大连假日风景", "Bearer e9095c89-6c4c-425e-a2b4-4dff853e7412");
         }
 
         private void AddUserProject(int userId, string userName, string projectCode, string projectName, string auth)
