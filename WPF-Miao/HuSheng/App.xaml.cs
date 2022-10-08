c5747911179ec784586db8bea893a4695e4ad1bd
fawei <50777393@qq.com>
2022-10-08 21:33:13 +0800

    Husheng - initial commit

---
+++ WPF-Miao/HuSheng/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using HttpProcessor.Container;
+using HuSheng.appointment;
+using HuSheng.search;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HuSheng
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            InitControllerAsync();
+        }
+
+        private void InitControllerAsync()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                InitController();
+            });
+        }
+        private void InitController()
+        {
+            HttpServiceController.AddTransientService<SearchController>();
+            HttpServiceController.AddTransientService<AppointController>();
+            //HttpServiceController.AddTransientService<YzmController>();
+
+            HttpServiceController.BuidServiceProvider();
+        }
+    }
+}
