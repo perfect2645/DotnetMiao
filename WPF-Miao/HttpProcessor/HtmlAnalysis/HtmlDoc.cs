c7e9b173131e0b783393d11d269e41be5adc6cd1
fawei <50777393@qq.com>
2022-10-09 23:30:25 +0800

    html agility

---
+++ WPF-Miao/HttpProcessor/HtmlAnalysis/HtmlDoc.cs
@@ -0,0 +1,50 @@
+﻿using HtmlAgilityPack;
+using HttpProcessor.ExceptionManager;
+
+namespace HttpProcessor.HtmlAnalysis
+{
+    public class HtmlDoc: IHtmlDoc
+    {
+        #region Properties
+
+        public string Html { get; private set; }
+
+        public HtmlDocument HtmlDocument { get; private set; }
+
+        public HtmlNode RootNode
+        { 
+            get { return HtmlDocument?.DocumentNode; }
+        }
+
+        #endregion Properties
+
+        #region Constructor
+
+        public HtmlDoc(string html)
+        {
+            Html = html;
+            BuildHtmlDoc();
+        }
+        private void BuildHtmlDoc()
+        {
+            if (string.IsNullOrEmpty(Html))
+            {
+                throw new HttpException("Build HtmlDoc failed, Html is Empty");
+            }
+
+            HtmlDocument = new HtmlDocument();
+            HtmlDocument.LoadHtml(Html);
+        }
+
+        #endregion Constructor
+
+        #region Html Node
+
+        public HtmlNodeCollection SearchNodes(string xpath)
+        {
+            return RootNode.SelectNodes(xpath);
+        }
+
+        #endregion Html Node
+    }
+}
