e508d3238990209334dab1479d63f0ce0790b4f0
50777393@qq.com <50777393@qq.com>
2022-10-14 16:41:06 +0800

    renren - initial commit

---
+++ WPF-Miao/renren/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using HttpProcessor.Container;
+using renren.appointment;
+using renren.search;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace renren
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            InitControllerAsync();
+        }
+
+        private void InitControllerAsync()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                InitController();
+            });
+        }
+        private void InitController()
+        {
+            HttpServiceController.AddTransientService<SearchController>();
+            HttpServiceController.AddTransientService<AppointController>();
+            //HttpServiceController.AddTransientService<YzmController>();
+
+            HttpServiceController.BuidServiceProvider();
+        }
+    }
+}
