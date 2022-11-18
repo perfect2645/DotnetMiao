ea8a15b293f46ea2603752b37ac4e38fe8de0f77
50777393@qq.com <50777393@qq.com>
2022-11-18 11:20:18 +0800

    南京雨花addyzm

--- WPF-Miao/Baohe/viewModel/platform/Jiankangzhilu.cs
+++ WPF-Miao/Baohe/viewModel/platform/Jiankangzhilu.cs
@@ -16,6 +16,7 @@ namespace Baohe.viewModel.platform
             HospitalName = hosName;
             DepartmentId = deptId;
             DepartmentName = deptName;
+            HasYzm = true;
         }
 
         public override string ToLogString()
