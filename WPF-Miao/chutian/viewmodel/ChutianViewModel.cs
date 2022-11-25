364910ab2f15c085df1057ab0dbb8980a8df22ee
fawei <50777393@qq.com>
2022-11-25 21:26:20 +0800

    chutian revert change

--- WPF-Miao/chutian/viewmodel/ChutianViewModel.cs
+++ WPF-Miao/chutian/viewmodel/ChutianViewModel.cs
@@ -128,10 +128,10 @@ namespace chutian.viewmodel
         private void TestData()
         {
             Interval = 800;
-            //StartTime = DateTime.Now.AddSeconds(20);
+            StartTime = DateTime.Now.AddSeconds(20);
             MainSession.PlatformSession.AddOrUpdate("StartTime", StartTime);
-            UserPhone = "17147715197";
-            _userPassword = "123456";
+            UserPhone = "13940897525";
+            _userPassword = "yinhen2645";
             //ScheduleId = "d900afa7-a1cd-427a-a601-dd5a51837609";
         }
 
