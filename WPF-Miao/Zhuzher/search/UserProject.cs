5aababb3a1061710518989c2aeee4f7e03e12b57
Fawei Cui <perfect200@163.com>
2022-11-27 16:14:36 +0800

    zhuzher - 1127

--- WPF-Miao/Zhuzher/search/UserProject.cs
+++ WPF-Miao/Zhuzher/search/UserProject.cs
@@ -24,8 +24,8 @@ namespace Zhuzher.search
 
         private void InitUserProjectList()
         {
-            //AddUserProject(21744666, "爸爸", "21020002", "大连假日风景", "Bearer 689461aa-2097-49c0-9d43-76074a47b7f4");
-            AddUserProject(21520695, "刘书辰", "21020025", "大连万科半山半海", "Bearer 988af8db-3523-404a-8d20-87291c2c7ae4");
+            AddUserProject(21744666, "爸爸", "21020002", "大连假日风景", "Bearer 689461aa-2097-49c0-9d43-76074a47b7f4");
+            //AddUserProject(21520695, "刘书辰", "21020025", "大连万科半山半海", "Bearer 988af8db-3523-404a-8d20-87291c2c7ae4");
             //AddUserProject(11067046, "Fawei", "21020002", "大连假日风景", "Bearer 56219e1c-ebfb-4d37-8c89-bd52afd57b25");
             //AddUserProject(13071092, "石琳", "21020025", "大连万科半山半海", "Bearer 8294733d-96c0-4536-9881-8106d09c1443");
             //AddUserProject(15045709, "妈妈", "21020025", "大连万科半山半海", "Bearer 4bfbf0d1-3379-4733-bea2-585570a388ad");
