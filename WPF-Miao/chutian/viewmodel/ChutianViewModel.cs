c9edd826e0a3aab828e89ab2ac29b1b93bd067b2
50777393@qq.com <50777393@qq.com>
2022-11-29 22:21:46 +0800

    chutian - temp for 20221130

--- WPF-Miao/chutian/viewmodel/ChutianViewModel.cs
+++ WPF-Miao/chutian/viewmodel/ChutianViewModel.cs
@@ -130,14 +130,14 @@ namespace chutian.viewmodel
             Interval = 800;
             StartTime = DateTime.Now.AddSeconds(20);
             MainSession.PlatformSession.AddOrUpdate("StartTime", StartTime);
-            UserPhone = "17014049717";
+            UserPhone = "17050685132";
             _userPassword = "123456";
             //ScheduleId = "d900afa7-a1cd-427a-a601-dd5a51837609";
         }
 
         private void InitStaticData()
         {
-            StartTime = new DateTime(2022, 11, 25, 8, 50, 56);
+            StartTime = new DateTime(2022, 11, 30, 8, 50, 56);
             MainSession.PlatformSession.AddOrUpdate("StartTime", StartTime);
 
             Departments = new List<HospitalDept>
