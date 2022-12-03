d7f21ae27aa83e631f9b467a1821a5d579e67bf9
Fawei Cui <perfect200@163.com>
2022-12-03 12:31:26 +0800

    cdgaoxin - init

---
+++ WPF-Miao/gaoxin/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using gaoxin.appointment;
+using gaoxin.search;
+using HttpProcessor.Container;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace gaoxin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            InitControllerAsync();
+        }
+
+        private void InitControllerAsync()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                InitController();
+            });
+        }
+        private void InitController()
+        {
+            HttpServiceController.AddTransientService<UserController>();
+            HttpServiceController.AddTransientService<SearchMiaoController>();
+            HttpServiceController.AddTransientService<YuyueController>();
+
+            HttpServiceController.BuidServiceProvider();
+        }
+    }
+}
