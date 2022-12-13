6548a5eac0c9af768a8ee24fae177efc789eef4a
50777393@qq.com <50777393@qq.com>
2022-12-13 17:38:16 +0800

    jkzl searcher - filter departments

--- WPF-Miao/JkzlSearcher/session/MainSession.cs
+++ WPF-Miao/JkzlSearcher/session/MainSession.cs
@@ -10,7 +10,7 @@ namespace JkzlSearcher.session
     {
         public static string Cookie { get; set; }
         public static Logger OutputLogger { get; private set; }
-        public static int CurrentHospitalId { get; set; } = 1000037;
+        public static int CurrentHospitalId { get; set; } = 1001194;
         private static readonly object _hospitalIdLock = new object();
         public static Dictionary<string, object> PlatformSession { get; private set; }
         static MainSession()
@@ -63,16 +63,17 @@ namespace JkzlSearcher.session
 
         internal static HashSet<string> InvalidDepartments = new HashSet<string>
         {
-            "外科", "内", "儿", "妇科", "口腔", "皮肤", "眼", "血液", "病", "症",
+            "外", "内", "儿", "妇科", "口腔", "皮肤", "眼", "血", "病", "症","神经",
             "耳喉","鼻","喉", "中医", "心", "针灸", "影像", "麻醉", "放射", "骨",
-            "急诊科", "诊断", "康复", "肾", "肿瘤", "疼痛", "氧", "胃",
-            "咨询", "精神", "失眠门诊", "理疗", "泌尿", "美容", "感染", "脑",
-            "老年", "病理", "乳腺", "脊柱", "重症", "产", "药剂", "检验", "消化",
+            "急诊", "诊断", "康复", "肾", "瘤", "疼痛", "氧", "胃", "门诊", "视",
+            "咨询", "精神", "理疗", "泌尿", "美容", "感染", "脑","乳房", "拿", "腹",
+            "老年", "病理", "乳腺", "脊柱", "重症", "产", "药", "检验", "消化",
             "医学", "白", "五官", "男", "痘", "银屑", "牛皮", "孕", "育", "牙",
-            "介", "肝", "呼吸", "体检", "急救", "肛", "肠", "微创", "中西",
+            "介", "肝", "呼吸", "体检", "急救", "肛", "肠", "微创", "中西", "湿",
             "性病", "激光", "哮喘", "失眠", "传染", "肌", "肺", "生殖", "热",
             "方便", "伤", "摩", "镜", "功能", "手", "足", "磁", "包", "超",
-            "胆", "石", "护理", "椎"
+            "胆", "石", "护理", "椎","干诊", "全科", "风", "循环", "镶", "营养",
+            "镇", "CT", "放疗", "ICU", "脉", "疤", "水疗", "特检", "整", "技", "移植"
         };
 
         #endregion Valid Department
