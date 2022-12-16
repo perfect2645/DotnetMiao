ac56e7f2a417ac9981f8db0b25a6764d3511af19
Fawei Cui <perfect200@163.com>
2022-12-16 20:03:18 +0800

    zhuzher - session

--- WPF-Miao/Zhuzher/search/UserProject.cs
+++ WPF-Miao/Zhuzher/search/UserProject.cs
@@ -26,9 +26,9 @@ namespace Zhuzher.search
         {
             //AddUserProject(15045709, "妈妈", "21020025", "大连万科半山半海", "Bearer f4df9196-345f-4d9a-9284-2a57735a7f32");
             //AddUserProject(21744666, "爸爸", "21020002", "大连假日风景", "Bearer ae4ddbc9-70b1-4c86-b529-6e696d0677de");
-            AddUserProject(13071092, "石琳", "21020025", "大连万科半山半海", "Bearer e6ee2700-b4b8-4cb8-9cc7-4937195921f2");
+            AddUserProject(13071092, "石琳", "21020025", "大连万科半山半海", "Bearer 2cf6ec91-19fe-499a-b39e-15af24d186d8");
             //AddUserProject(21520695, "刘书辰", "21020025", "大连万科半山半海", "Bearer d33f1b76-9635-4e65-b320-e6fd5cc5be5c");
-            //AddUserProject(11067046, "Fawei", "21020002", "大连假日风景", "Bearer 4dddffae-c5d0-4a4d-87d6-ee481120515e");
+            //AddUserProject(11067046, "Fawei", "21020002", "大连假日风景", "Bearer 21ff89ab-90a7-4378-979c-2095b91fb239");
             //AddUserProject(12513073, "Zoe", "21020012", "大连樱花园", "Bearer d91f2b58-fa2c-4f4a-9b13-b9a135105596");
 
 
