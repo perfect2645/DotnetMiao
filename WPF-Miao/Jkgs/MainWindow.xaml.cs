09c422243d404a119f665e82145a3a48c1b87af2
Fawei Cui <perfect200@163.com>
2022-12-17 14:39:34 +0800

    jkgs - init

---
+++ WPF-Miao/Jkgs/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Jkgs.viewmodel;
+using System;
+using System.Windows;
+
+namespace Jkgs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                DataContext = new JkgsViewModel(mainConsole.baseConsole.LogPanel);
+            }
+            catch (Exception ex)
+            {
+                Logging.GLog.Logger.Error(ex);
+            }
+        }
+    }
+}
