9508514b55d302303343c747b58be7388eca3d1f
Fawei Cui <perfect200@163.com>
2023-01-14 15:15:20 +0800

    zhuzher - 2023 chunjie

--- WPF-Miao/Zhuzher/search/SunActivityScence.cs
+++ WPF-Miao/Zhuzher/search/SunActivityScence.cs
@@ -21,26 +21,9 @@ namespace Zhuzher.search
 
         private void InitSceneList()
         {
-            //AddScene("app-act-invite&view", 3, "1");
+            AddScene("app-act-invite&view", 9, "1");
             //AddScene("app-act-help", 1, "1");
-            //AddScene("doc-withhold&prepay12months", 1, "zze://vanke.com/common/reactnative?route=BillRoute/PayBills");
-            //AddScene("doc-prepay12months", 1, "zze://vanke.com/common/reactnative?route=BillRoute/PayBills");
-            //AddScene("doc-prepay12months", 1, "zze://vanke.com/common/reactnative?route=BillRoute/PayBills");
-            //AddScene("doc-withhold&prepay24months", 1, "zze://vanke.com/common/reactnative?route=BillRoute/PayBills");
-            //AddScene("doc-withhold&prepay36months", 1, "zze://vanke.com/common/reactnative?route=BillRoute/PayBills");
-            //AddScene("app-startapp", 1, "1");
-            // 首次进入活动，金币+5，并额外获得5个夺宝机会
-            // 最多10次 组队邀请好友，好友成功加入队伍，则算完成
-            //AddScene("app-act-invite&jointeam", 1, "1");
-
-            //住这儿友邻市集单笔消费100元以上，每日可完成1次
-            AddScene("5thzone-blackpearl-pay", 5, "https://uiis.4009515151.com/activityTopic/4666?pageMark=ptgejhd");
-
-            //优惠下单购买油烟机清洗服务！每日可完成1次任务 "limitCount": 16,
-            //AddScene("5thzone-convenience-pay", 1, "https://neighbor.4009515151.com/summoner-canary/orders/product-detail?pageMark=zzesjbyyjdp&goodsId=6617274");
-
-            //在世界杯话题下发帖即可完成，每日1次，每次+2金币 "limitCount": 2,
-            //AddScene("app-post", 1, "https://enterprise.4009515151.com/marketing-pages/forum/topicPage?id=224");
+            //AddScene("doc-withhold&prepay12months", 1, "zze://vanke.com/common/reactnative?route=BillRoute/PayBills");;
 
         }
 
--- WPF-Miao/Zhuzher/search/UserProject.cs
+++ WPF-Miao/Zhuzher/search/UserProject.cs
@@ -24,11 +24,11 @@ namespace Zhuzher.search
 
         private void InitUserProjectList()
         {
-            AddUserProject(11067046, "Fawei", "21020002", "大连假日风景", "Bearer 80ae150e-5ec9-4536-97ec-1bac5a4546a7");
-            AddUserProject(15045709, "妈妈", "21020025", "大连万科半山半海", "Bearer 02bf2b18-4550-40f2-ab09-c48cad06f883");
-            AddUserProject(21744666, "爸爸", "21020002", "大连假日风景", "Bearer bc578200-eaf7-4518-a5dd-cf31687b5585");
-            AddUserProject(13071092, "石琳", "21020025", "大连万科半山半海", "Bearer 3246aa0e-52f1-472d-8b39-d17bf986c6e7");
-            AddUserProject(21520695, "刘书辰", "21020025", "大连万科半山半海", "Bearer 04c3d6fc-0b6d-4ce7-817c-7aa0f0bd90d6");
+            AddUserProject(11067046, "Fawei", "21020002", "大连假日风景", "Bearer e6eea748-f688-4a24-8cb4-b929b00349eb");
+            //AddUserProject(15045709, "妈妈", "21020025", "大连万科半山半海", "Bearer 02bf2b18-4550-40f2-ab09-c48cad06f883");
+            AddUserProject(21744666, "爸爸", "21020002", "大连假日风景", "Bearer 4221de6d-2780-4ae7-86b2-f0c5ebec4d58");
+            //AddUserProject(13071092, "石琳", "21020025", "大连万科半山半海", "Bearer 3246aa0e-52f1-472d-8b39-d17bf986c6e7");
+            //AddUserProject(21520695, "刘书辰", "21020025", "大连万科半山半海", "Bearer 04c3d6fc-0b6d-4ce7-817c-7aa0f0bd90d6");
             //AddUserProject(12513073, "Zoe", "21020012", "大连樱花园", "Bearer d91f2b58-fa2c-4f4a-9b13-b9a135105596");
 
             //AddUserProject(14929048, "Gary", "21020012", "大连樱花园", "Bearer d19fc1cd-7efd-4bb2-a1cb-af42d92cf051");
