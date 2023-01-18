6bb732a27d49908cb37bdfd6befe54bf3f15bb49
Fawei <50777393@qq.com>
2023-01-18 20:25:10 +0800

    shengzhi - create project

---
+++ WPF-Miao/Shengzhi/appointment/YuyueContent.cs
@@ -0,0 +1,28 @@
+﻿using Shengzhi.common;
+using Shengzhi.login;
+using Utils;
+
+namespace Shengzhi.appointment
+{
+    internal class YuyueContent : ShengzhiContent
+    {
+        private static string url = "https://ldsq.ldrmyy120.com/rest/v1/api/examine/vaccine_created/";
+        public Order Order { get; private set; }
+        public YuyueContent(Order order, ShengzhiLogin user) : base(url, user)
+        {
+            Order = order;
+            BuildContent();
+        }
+
+        private void BuildContent()
+        {
+            AddContent("duty_time_id", Order.DutyTimeId);
+            AddContent("vaccine_id", Order.VaccineId);
+            AddContent("hospital", Order.HosipitalId);
+            AddContent("inoculate_times", Order.InoculateTimes);
+            AddContent("patient", Order.UserId);
+            AddContent("see_date", Order.SeeDate);
+            AddContent("address", Order.Address);
+        }
+    }
+}
---
+++ WPF-Miao/Shengzhi/viewmodel/ShengzhiHospital.cs
@@ -0,0 +1,20 @@
+﻿using Base.viewModel.hospital;
+using System.Text;
+
+namespace Shengzhi.viewmodel
+{
+    internal class ShengzhiHospital : HospitalDept
+    {
+        public override string ToLogString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("********河北生殖妇产医院-选择医院科室*********");
+            sb.AppendLine($"{HospitalName}- {DepartmentName}");
+            sb.AppendLine($"DepartmentId - {DepartmentId}");
+            sb.AppendLine("**************************************");
+
+            return sb.ToString();
+        }
+    }
+}
