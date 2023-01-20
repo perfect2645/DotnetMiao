90e7a4962c8116712a73349c2bd70fbe3aaa4b7b
Fawei Cui <perfect200@163.com>
2023-01-20 23:58:15 +0800

    shengzhi - order

--- WPF-Miao/Shengzhi/appointment/Order.cs
+++ WPF-Miao/Shengzhi/appointment/Order.cs
@@ -1,19 +1,23 @@
-﻿using Base.Events;
-using System;
+﻿using Shengzhi.login;
 using System.Text;
-using Shengzhi.login;
-using Utils.timerUtil;
 
 namespace Shengzhi.appointment
 {
     public class Order
     {
+        public string Amount { get; set; }
         public string AppointScore { get; set; }
         public string AppUuid { get; set; }
         public string ChannelId { get; set; }
+        public string DeptCode { get; set; }
         public string GroupCode { get; set; }
+        public string HidEncode { get; set; }
         public string HospitalWxOpenId { get; set; }
         public string ImeiId { get; set; }
+        public string IsCreateCard { get; set; }
+        public string MarkDescEncode { get; set; }
+        public string PatientId { get; set; }
+        public string Phone { get; set; }
         public string PhoneOperationSys { get; set; }
         public string PhoneType { get; set; }
         public string PhoneVersionNum { get; set; }
@@ -23,10 +27,10 @@ namespace Shengzhi.appointment
         public string UserName { get; set; }
         public string UserVsId { get; set; }
         public string HospitalId { get; set; }
-        public string MonitorRecords { get; set; }
         public string OpVersion { get; set; }
         public string OperateCurrentUserId { get; set; }
         public string OperateUserSource { get; set; }
+        public string PostData { get; set; }
         internal ShengzhiLogin User { get; set; }
 
         public string ToLogString()
