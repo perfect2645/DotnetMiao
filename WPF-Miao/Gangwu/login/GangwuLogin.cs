dff58dbedccc32d2f9669bc8bc2a8f3123e6425a
Fawei <50777393@qq.com>
2023-02-15 23:29:44 +0800

    港务init

---
+++ WPF-Miao/Gangwu/login/GangwuLogin.cs
@@ -0,0 +1,9 @@
+﻿namespace Gangwu.login
+{
+    internal class GangwuLogin
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ WPF-Miao/Gangwu/session/MainSession.cs
@@ -0,0 +1,51 @@
+﻿using Base.session;
+using Base.viewmodel.status;
+using Gangwu.appointment;
+using Gangwu.login;
+using System.Collections.Generic;
+
+namespace Gangwu.session
+{
+    public class MainSession : MainSessionBase, ISessionContainer
+    {
+        public static string Cookie { get; set; }
+        public static Dictionary<string, object> PlatformSession { get; private set; }
+        internal static List<GangwuLogin> Users { get; set; }
+        public static OrderEvent OrderEvent { get; }
+        public static AppointSession AppointSession { get; private set; }
+        
+        internal static Dictionary<string, Order> Orders { get; set; }
+
+        static MainSession()
+        {
+            PlatformSession = new Dictionary<string, object>();
+            OrderEvent = new OrderEvent();
+            Users = new List<GangwuLogin>();
+            Orders = new Dictionary<string, Order>();
+        }
+
+        public static void InitSession()
+        {
+            AppointSession = new AppointSession();
+        }
+
+        #region MiaoStatus
+
+        public static void SetStatus(MiaoProgress miaoProgress)
+        {
+            MiaoStatus.MiaoProgress = miaoProgress;
+        }
+
+        public static MiaoProgress GetStatus()
+        {
+            return MiaoStatus.MiaoProgress;
+        }
+
+        public static void SetStatus(MiaoProgress miaoProgress, object data)
+        {
+            MiaoStatus.OnStatusUpdate(miaoProgress, data);
+        }
+
+        #endregion MiaoStatus
+    }
+}
