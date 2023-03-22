23d056da4640bcc16b95772efb6e8b79605b03d7
Fawei <50777393@qq.com>
2023-03-22 19:56:05 +0800

    zhangzhou

--- WPF-Miao/Baohe/viewModel/BaoheViewModel.cs
+++ WPF-Miao/Baohe/viewModel/BaoheViewModel.cs
@@ -95,7 +95,7 @@ namespace Baohe.viewModel
         private void InitStaticData()
         {
             //StartTime = DateTime.Today.AddHours(11).AddMinutes(59).AddSeconds(58);
-            StartTime = DateTime.Today.AddHours(7).AddMinutes(59).AddSeconds(58);
+            StartTime = DateTime.Today.AddHours(5).AddMinutes(59).AddSeconds(58);
             //StartTime = DateTime.Today.AddHours(19).AddMinutes(59).AddSeconds(58);
             //StartTime = DateTime.Today.AddHours(21).AddMinutes(59).AddSeconds(58);
             //StartTime = DateTime.Today.AddHours(20).AddMinutes(29).AddSeconds(58);
