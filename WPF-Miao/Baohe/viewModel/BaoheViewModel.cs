92ad5d926e5fec1eac048f39d5723a7b03007b58
Fawei <50777393@qq.com>
2023-03-26 23:22:34 +0800

    0327 - wuhan

--- WPF-Miao/Baohe/viewModel/BaoheViewModel.cs
+++ WPF-Miao/Baohe/viewModel/BaoheViewModel.cs
@@ -89,17 +89,18 @@ namespace Baohe.viewModel
 
             //VerifyCode.ArrangeSn = "169149843";
             //VerifyCode.Phone = "18301135103";
-            StartTime = DateTime.Now.AddSeconds(20);
+            //StartTime = DateTime.Now.AddSeconds(20);
         }
 
         private void InitStaticData()
         {
             MainSession.YzmMode = YzmMode.OnTimeSendVerify;
 
+            //StartTime = DateTime.Today.AddHours(5).AddMinutes(59).AddSeconds(58);
             //StartTime = DateTime.Today.AddHours(7).AddMinutes(59).AddSeconds(58);
             //StartTime = DateTime.Today.AddHours(11).AddMinutes(59).AddSeconds(58);
-            //StartTime = DateTime.Today.AddHours(5).AddMinutes(59).AddSeconds(58);
-            StartTime = DateTime.Today.AddHours(19).AddMinutes(59).AddSeconds(58);
+            StartTime = DateTime.Today.AddHours(15).AddMinutes(59).AddSeconds(58);
+            //StartTime = DateTime.Today.AddHours(19).AddMinutes(59).AddSeconds(58);
             //StartTime = DateTime.Today.AddHours(20).AddMinutes(29).AddSeconds(58);
             //StartTime = DateTime.Today.AddHours(21).AddMinutes(59).AddSeconds(58);
 
