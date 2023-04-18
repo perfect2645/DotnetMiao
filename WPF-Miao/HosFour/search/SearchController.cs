d8760b4f855067eb1dce87d9b5bf0999575e3c5c
Fawei <50777393@qq.com>
2023-04-18 23:21:25 +0800

    hos4 - search controller

--- WPF-Miao/HosFour/search/SearchController.cs
+++ WPF-Miao/HosFour/search/SearchController.cs
@@ -11,50 +11,13 @@ namespace HosFour.search
 {
     internal class SearchController
     {
-        private DateController dateController;
-        //private IntervalOnTime SearchInterval;
-
         public SearchController()
         {
-            dateController = HttpServiceController.GetService<DateController>();
-            //SearchInterval = new IntervalOnTime(SearchAsync, "search", 200);
-        }
-
-        public void StartSearchInterval()
-        {
-            MainSession.SetStatus(MiaoProgress.GettingMiao);
-            //SearchInterval.StartIntervalOntime();
         }
 
         public void SearchMiao()
         {
             MainSession.SetStatus(MiaoProgress.GettingMiao);
-
-            GetDates();
-            GetTimes();
-        }
-
-        private void GetDates()
-        {
-            var defaultUser = MainSession.Users.FirstOrDefault();
-
-            var isDateGet = false;
-            while (!isDateGet)
-            {
-                isDateGet = dateController.GetDate(defaultUser);
-                Thread.Sleep(500);
-            }
-        }
-
-        private void GetTimes()
-        {
-            var dateList = MainSession.PlatformSession["orderDates"] as List<string>;
-            var defaultUser = MainSession.Users.FirstOrDefault();
-
-            foreach (var date in dateList)
-            {
-                Task.Factory.StartNew(() => GetMiao(date, defaultUser));
-            }
         }
 
         private void GetMiao(string date, HosFourLogin user)
