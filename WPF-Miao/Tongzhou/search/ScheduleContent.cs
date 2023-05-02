d52204fc15f60d2d7bdee498b8da49cb2de850ba
fawei <50777393@qq.com>
2023-05-02 23:13:02 +0800

    tongzhou - schedule

---
+++ WPF-Miao/Tongzhou/search/ScheduleContent.cs
@@ -0,0 +1,32 @@
+﻿using Tongzhou.common;
+using Tongzhou.login;
+using Tongzhou.session;
+using Utils;
+
+namespace Tongzhou.search
+{
+    internal class ScheduleContent : TongzhouPostContent
+    {
+        public ScheduleContent(TongzhouLogin user) : base("queryDoctorSchedulingByDateV2", "appoint.scheduleService", user)
+        {
+            BuildContent();
+            BuildSignHeaders();
+        }
+
+        private void BuildContent()
+        {
+            var organId = MainSession.PlatformSession.GetString(Constants.HospitalId);
+            var doctorId = MainSession.PlatformSession.GetString(Constants.DoctorId);
+
+            AddContent("organId", organId);
+            AddContent("appointDepartCodes", new string[]{ });
+            AddContent("sourceLevel", null);
+            AddContent("hisSourceLevelText", null);
+            AddContent("busType", null);
+            AddContent("sourceType", 1);
+            AddContent("doctorId", doctorId);
+            AddContent("date", null);
+            AddContent("scheduleDesc", null);
+        }
+    }
+}
---
+++ WPF-Miao/Tongzhou/search/ScheduleController.cs
@@ -0,0 +1,63 @@
+﻿using HttpProcessor.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Tongzhou.common;
+using Tongzhou.login;
+using Tongzhou.session;
+using Utils;
+using Utils.json;
+
+namespace Tongzhou.search
+{
+    internal class ScheduleController : HttpClientBase
+    {
+        public ScheduleController(HttpClient httpClient) : base(httpClient)
+        {
+        }
+        public void GetScheduleAsync(TongzhouLogin user)
+        {
+            Task.Factory.StartNew(() => GetSchedule(user));
+        }
+
+        private void GetSchedule(TongzhouLogin user)
+        {
+            try
+            {
+                var content = new UserContent(user);
+                content.BuildDefaultHeaders(Client);
+                var response = PostStringAsync(content, HttpProcessor.Content.ContentType.Array).Result;
+                if (response?.ContentStr == null)
+                {
+                    MainSession.PrintLogEvent.Publish(this, $"GetUser - {response?.Message},请检查参数");
+                    return;
+                }
+
+                var dicResultDe = Encrypt.Decrypt22(response?.ContentStr);
+                var dicResult = JsonAnalysis.JsonToDic(dicResultDe);
+
+                CheckSaveUser(dicResult, user);
+            }
+            catch (Exception ex)
+            {
+                MainSession.PrintLogEvent.Publish(this, $"获取用户信息异常{ex.Message}");
+            }
+        }
+
+        private bool CheckSaveUser(Dictionary<string, object> dicResult, TongzhouLogin user)
+        {
+            var code = dicResult.GetString("code").ToInt();
+            if (code != 200)
+            {
+                var msg = dicResult.GetString("msg");
+                MainSession.PrintLogEvent.Publish(this, $"Get Userinfo Failed.code={code}, msg={msg}");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
