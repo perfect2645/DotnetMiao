ab70de164108136df454884d6977129eb4d1d2ad
Fawei <50777393@qq.com>
2023-06-01 08:55:11 +0800

    resission * 6

--- WPF-Miao/Base/viewModel/OnTimeViewModel.cs
+++ WPF-Miao/Base/viewModel/OnTimeViewModel.cs
@@ -168,12 +168,12 @@ namespace Base.viewModel
             {
                 if (ReSessionTimer == null)
                 {
-                    ReSessionTimer = new IntervalOnTime(ReSession, "刷新登录信息", DateTime.Now, 600000);
+                    ReSessionTimer = new IntervalOnTime(ReSession, "刷新登录信息", DateTime.Now, 600000 * 6);
                     return;
                 }
 
                 ReSessionTimer.StopInterval();
-                ReSessionTimer.ResetTimer(DateTime.Now, 600000);
+                ReSessionTimer.ResetTimer(DateTime.Now, 600000 * 6);
             }
             catch (Exception ex)
             {
