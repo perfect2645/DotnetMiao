f5042a39e0ef33c3c06552fb338524b2a1892c18
fawei <50777393@qq.com>
2023-07-22 15:43:05 +0800

    6water - schedule

--- WPF-Miao/SixWater/search/MiaoContent.cs
+++ WPF-Miao/SixWater/search/MiaoContent.cs
@@ -1,26 +1,23 @@
 ﻿using SixWater.common;
 using SixWater.login;
 using SixWater.session;
+using System;
 using Utils;
 
 namespace SixWater.search
 {
     internal class MiaoContent : SixWaterContent
     {
-        private static string baseUrl = "https://ljzyyapi.yuanbaodaojia.com/v1/vaccine_day_list";
+        private static string baseUrl = "https://oss.zsqrmyy.com:8443/patient/register/doctorScheduledRecords?doctorId=&deptId=781&doctorScheduleId=50628&orgId=1478314809638342658&dateDivision=&scheduleDate=2023-07-22";
 
-        public MiaoContent(SixWaterLogin user) : base(baseUrl, user)
+        public MiaoContent(SixWaterLogin user, string date, string scheduleId) : base(baseUrl, user)
         {
-            BuildContent();
+            BuildUrl();
         }
 
-        private void BuildContent()
+        private void BuildUrl()
         {
-            var hosId = MainSession.PlatformSession.GetString(Constants.HospitalId);
-            var deptId = MainSession.PlatformSession.GetString(Constants.DeptId);
-            AddContent("vaccineId", deptId);
-            AddContent("vaccineAddressId", hosId);
-            AddContent("token", User.Authorization);
+            var doctorId = MainSession.PlatformSession.GetString(Constants.DoctorId);
         }
     }
 }
