c21eb988ed337335aaac0d00ff49f7861bb135d4
Fawei <50777393@qq.com>
2023-07-29 12:39:48 +0800

    zhuzher

--- WPF-Miao/Zhuzher/search/SunActivityScence.cs
+++ WPF-Miao/Zhuzher/search/SunActivityScence.cs
@@ -22,6 +22,7 @@ namespace Zhuzher.search
         private void InitSceneList()
         {
             AddScene("app-startapp", 1, "1");
+            AddScene("puli-daka", 1, "zze://vanke.com/common/service/launch_miniProgram?program_id=gh_501eecfa8fff&path=pages%2FMilkBook%2Findex%3Furl%3Dhttps%3A%2F%2Fmp.weixin.qq.com%2Fs%2Fz0lWLJ8LGFeGAOLEpi1DuA");
             AddScene("app-points", 1, "https://enterprise.4009515151.com/marketing-pages/integral/turnTable?from=hd0714");
             AddScene("app-post-vote", 1, "https://enterprise.4009515151.com/marketing-pages/forum/topicPage?id=431&from=activity914");
             AddScene("app-post", 1, "https://enterprise.4009515151.com/marketing-pages/forum/topicPage?id=430&from=activity");
