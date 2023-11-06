c49fc469a1538486bac233e52437c7a0cefc0fdc
Fawei <50777393@qq.com>
2023-11-06 19:34:36 +0800

    frida

--- WPF-Miao/Zhuzher/search/UserProject.cs
+++ WPF-Miao/Zhuzher/search/UserProject.cs
@@ -24,13 +24,13 @@ namespace Zhuzher.search
 
         private void InitUserProjectList()
         {
-            AddUserProject(11067046, "Fawei", "21020025", "大连万科半山半海", "Bearer b138f0c0-c9b9-43a4-aa9a-6ae7d68ecd37");
-            AddUserProject(15045709, "妈妈", "21020002", "大连假日风景", "Bearer 37e45690-098a-460c-b940-570081f3d5a8");
-            AddUserProject(24521842, "Fawei2645", "21020025", "大连万科半山半海", "Bearer bea0310e-f57c-46c3-8894-7e5b924ec2b8");
+            //AddUserProject(11067046, "Fawei", "21020025", "大连万科半山半海", "Bearer b138f0c0-c9b9-43a4-aa9a-6ae7d68ecd37");
+            //AddUserProject(15045709, "妈妈", "21020002", "大连假日风景", "Bearer 37e45690-098a-460c-b940-570081f3d5a8");
+            //AddUserProject(24521842, "Fawei2645", "21020025", "大连万科半山半海", "Bearer bea0310e-f57c-46c3-8894-7e5b924ec2b8");
             //AddUserProject(13071092, "石琳", "21020025", "大连万科半山半海", "Bearer 6a5e93cc-6829-44e4-8f9e-3683f9a6f79c");
             AddUserProject(21520695, "刘书辰", "21020025", "大连万科半山半海", "Bearer e63bb539-8b8f-459c-a561-e4d9666af2cc");
-            AddUserProject(26190961, "7719", "21020002", "大连假日风景", "Bearer 71624839-84c4-4fd3-ae88-bf8f15e7082d");
-            AddUserProject(21744666, "爸爸", "21020002", "大连假日风景", "Bearer a37d0104-cef8-416e-be4c-b0b0e44117a3");
+            //AddUserProject(26190961, "7719", "21020002", "大连假日风景", "Bearer 71624839-84c4-4fd3-ae88-bf8f15e7082d");
+            //AddUserProject(21744666, "爸爸", "21020002", "大连假日风景", "Bearer a37d0104-cef8-416e-be4c-b0b0e44117a3");
 
             //AddUserProject(14929048, "Gary", "21020012", "大连樱花园", "Bearer d19fc1cd-7efd-4bb2-a1cb-af42d92cf051");
         }
--- android/doc/CodeAnalysis/Frida.md
+++ android/doc/CodeAnalysis/Frida.md
@@ -14,3 +14,47 @@ pip install frida-tools==9.2.5
 - cmd 中执行 python，进入控制台 import frida，能导包成功说明没问题
 
 ![](imgs/2023-10-29-20-21-23.png)
+
+## Frida 代码提示
+
+npm i @types/frida-gum
+
+## Frida Server
+
+[Frida Server 指定版本下载](https://github.com/frida/frida/releases/tag/14.2.18)
+
+- 通过 usb 连真机不需要转发端口，通过 ip 连接需要转发，连模拟器也需要转发
+
+  > adb forward tcp:27042 tcp:27042
+
+- push frida server package to your phone
+
+```cmd
+
+C:\Users\Fawei>adb push F:\dev-software\android\frida\frida-server-14.2.18-android-arm64 /data/local/tmp/fsarm64
+```
+
+- CD to target folder
+
+```
+C:\Users\Fawei>adb shell
+sailfish:/ $ su
+sailfish:/ # cd data/local/tmp/fsarm64
+/system/bin/sh: cd: /data/local/tmp/fsarm64: Not a directory
+2|sailfish:/ # cd data/local/tmp
+sailfish:/data/local/tmp # ls
+fsarm64 lldb-server start_lldb_server.sh
+sailfish:/data/local/tmp #
+```
+
+- 修改权限
+
+```
+sailfish:/data/local/tmp # chmod 777 fsarm64
+```
+
+- 运行 Frida Server
+
+```
+./fsarm64
+```
