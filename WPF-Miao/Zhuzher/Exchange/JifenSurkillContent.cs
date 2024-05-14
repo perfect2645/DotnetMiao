8d67f1e4d2785ae7f967dafe372d0f2e93e5cac7
Fawei <50777393@qq.com>
2024-05-14 22:53:59 +0800

    zhuzher- scoresurkill v3

--- WPF-Miao/Zhuzher/Exchange/JifenSurkillContent.cs
+++ WPF-Miao/Zhuzher/Exchange/JifenSurkillContent.cs
@@ -1,17 +1,17 @@
 ﻿using HttpProcessor.Content;
 using Utils.datetime;
 using Utils.stringBuilder;
+using Zhuzher.Common;
 using Zhuzher.search;
 using Zhuzher.session;
 
 namespace Zhuzher.Exchange
 {
-    internal class JifenSurkillContent : HttpStringContent
+    internal class JifenSurkillContent : OnewoContent
     {
-        public UserProject User { get; set; }
         public ScoreItem ScoreItem { get; set; }
-        public const string Url = "https://chaos.4009515151.com/market/api/score/time/exchange";
-        public JifenSurkillContent(UserProject user, ScoreItem scoreItem) : base(Url)
+        public const string Url = "https://z.onewo.com/market/api/score/time/exchange";
+        public JifenSurkillContent(UserProject user, ScoreItem scoreItem) : base(Url, user)
         {
             User = user;
             ScoreItem = scoreItem;
@@ -21,16 +21,6 @@ namespace Zhuzher.Exchange
 
         private void BuildHeader()
         {
-            AddHeader("Host", "chaos.4009515151.com");
-            AddHeader("Origin", "https://enterprise.4009515151.com");
-            AddHeader("Accept-Language", "zh-CN,zh-Hans;q=0.9");
-            AddHeader("Accept-Encoding", "gzip, deflate, br");
-            AddHeader("Connection", "keep-alive");
-            AddHeader("Accept", "application/json");
-            AddHeader("User-Agent", "Mozilla/5.0 AppleWebKit/605.1.15 vanke_app_version/5.3.92 X_API_VERSION/20220902 vanke_app/zhuzher vanke_jsbridge_version/5.3.92");
-            AddHeader("Authorization", User.Authorization);
-            AddHeader("Referer", "https://enterprise.4009515151.com/");
-
         }
 
         private void BuildContent()
