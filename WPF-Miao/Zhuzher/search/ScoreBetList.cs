bd6331d047c06226a24dc4a07abf4f6b71f30993
Fawei <50777393@qq.com>
2024-05-18 11:45:06 +0800

    zhuzher 0518

--- WPF-Miao/Zhuzher/search/ScoreBetList.cs
+++ WPF-Miao/Zhuzher/search/ScoreBetList.cs
@@ -15,7 +15,16 @@ namespace Zhuzher.search
         private void InitList()
         {
             ScoreBets = new List<ScoreBet>();
-            ScoreBets.Add(new ScoreBet(56, 8431, 20, "Apple iPhone 15 支持移动联通电信5G"));
+            ScoreBets.Add(new ScoreBet(56, 8986, 100, "樱桃1"));
+            ScoreBets.Add(new ScoreBet(56, 8987, 100, "樱桃2"));
+            ScoreBets.Add(new ScoreBet(56, 8988, 100, "樱桃3"));
+            ScoreBets.Add(new ScoreBet(56, 8989, 100, "樱桃4"));
+            ScoreBets.Add(new ScoreBet(56, 8990, 100, "樱桃5"));
+            ScoreBets.Add(new ScoreBet(56, 8991, 100, "樱桃6"));
+            ScoreBets.Add(new ScoreBet(56, 8992, 100, "樱桃7"));
+            ScoreBets.Add(new ScoreBet(56, 8993, 100, "樱桃8"));
+            ScoreBets.Add(new ScoreBet(56, 8994, 100, "樱桃9"));
+            ScoreBets.Add(new ScoreBet(56, 8995, 100, "樱桃10"));
 
         }
     }
