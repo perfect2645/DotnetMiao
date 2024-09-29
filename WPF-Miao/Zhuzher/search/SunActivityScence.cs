76d145605820c01339dd1d17b1e83c4d043c7e7c
Fawei Cui <perfect200@163.com>
2024-09-29 18:17:40 +0800

    zhuzher-0929

--- WPF-Miao/Zhuzher/search/SunActivityScence.cs
+++ WPF-Miao/Zhuzher/search/SunActivityScence.cs
@@ -21,17 +21,21 @@ namespace Zhuzher.search
         public List<SunActivityScence> ScenceList = new List<SunActivityScence>();
         public SunActivityScenceList()
         {
-            InitSceneList();
-            //TestSceneList();
+            //InitSceneList();
+            TestSceneList();
         }
 
         private void TestSceneList()
         {
             ////成功加入别人队伍
             //AddScene("app-act-jointeam", 1, "1");
-
+            ////转发活动到微信群
+            //AddScene("app-h5share", 1, "", 3);
             ////关注公众号每日领阳光
             //AddScene("app-h5share", "https://enterprise.4009515151.com/fe-event-pages/marketing?type=mini&id=1105&scene=gzhanswer&from=gzhanswer");
+
+            //关注公众号每日领阳光
+            AddScene("puli-daka", "gzhanswer");
 
         }
 
@@ -128,10 +132,7 @@ namespace Zhuzher.search
             AddScene("app-browseweb", 1, "1");
 
             //转发活动到微信群
-            AddScene("app-h5share", 2, "", 3);
-
-            //转发活动到微信群
-            AddScene("app-h5share", 2, "", 3);
+            AddScene("app-h5share", 1, "", 3);
 
             //关注公众号每日领阳光
             AddScene("puli-daka", "gzhanswer");
